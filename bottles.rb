01cff848a6627b317e744784bfb8cbc24b214550
ShyamliC <shyamli.channa@gmail.com>
2018-07-16 14:40:57 +0000

    automatically backed up by learn

--- bottles.rb
+++ bottles.rb
@@ -2,10 +2,10 @@ bottles=99
 
 while bottles>=1
 
-if bottles>1 
-  puts "#{bottles} bottles of RootBeer on the wall, #{bottles} of RootBeer, Take one down and pass it around, #{bottles-1} bottles of RootBeer on the wall"
-  bottles-=1 
+if bottles==1 
+  puts "#{bottles} of RootBeer on the wall, #{bottles} of RootBeer. Go to the store and buy some more, 99 bottles of RootBeer on the wall"
 else
-  puts "#{bottles} of RootBeer on the wall, #{bottles} of RootBeer. Go to the store and but some more, 99 bottles of RootBeer on the wall"
+  puts "#{bottles} bottles of RootBeer on the wall, #{bottles} of RootBeer, Take one down and pass it around, #{bottles-1} bottles of RootBeer on the wall"
 end
-end 
+bottles-=1
+end
